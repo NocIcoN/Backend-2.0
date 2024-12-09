20df389a022f19f82b2c6e81e588c32e35d5f91c
NocIcoN <90994680+NocIcoN@users.noreply.github.com>
2024-12-09 19:15:42 +0700

    remove middleware

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const { getAllUsers, getUserById, deleteUser, getUserProfile, updateUserProfile } = require('../controllers/userController'); 
-const { protect, admin } = require('../middleware/authMiddleware');
+// const { protect, admin } = require('../middleware/authMiddleware');
 
 // Get all users (admin only)
-router.get('/', protect, admin, getAllUsers);
+router.get('/', /** protect, admin, */ getAllUsers);
 
 // Get specific user by ID (admin or user)
-router.get('/:id', protect, getUserById);
+router.get('/:id',/**  protect,*/ getUserById);
 
 // Update user details (admin or user)
-router.put('/:id', protect, updateUserProfile); 
+router.put('/:id',/**  protect,*/ updateUserProfile); 
 
 // Delete user (admin only)
-router.delete('/:id', protect, admin, deleteUser);
+router.delete('/:id',/**  protect, admin,*/ deleteUser);
 
 // Get logged-in user profile
-router.get('/profile', protect, getUserProfile);
+router.get('/profile',/**  protect,*/ getUserProfile);
 
 module.exports = router;
